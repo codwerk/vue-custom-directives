79c987062da8ba53d5ea69a4567484d53ab81a59
codwerk <louis_widdy@live.fr>
2020-07-06 18:34:00 +0200

    create directive

--- src/main.js
+++ src/main.js
@@ -3,6 +3,40 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+//Custom directives
+
+Vue.directive('second', {
+  bind(el, binding) {
+    console.log(el);
+    
+    if(binding.arg == 'column') {
+      el.style.background = 'blue';
+      el.style.padding = '20px';
+    }
+  }
+})
+
+Vue.directive('theme', {
+  bind(el,binding) {
+    console.log(el);
+    
+    
+    if(binding.value == "wide") {
+      el.style.maxWidth = '1200px';
+      el.style.margin = 'auto';
+    } else if (binding.value == 'narrow') {
+      el.style.background= 'aqua';
+    }
+  }
+})
+
+Vue.directive('colorLi', {
+  bind(el) {
+    el.style.color= "#"+Math.random().toString().slice(2, 8);
+  }
+})
+
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
