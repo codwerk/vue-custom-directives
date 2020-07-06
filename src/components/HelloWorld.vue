59389e7cb43e3d22979e820eff5325d15580dacd
codwerk <louis_widdy@live.fr>
2020-07-06 18:31:11 +0200

    add directive on html

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="hello" v-theme="'narrow'">
+    <h1 >{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3 v-colorLi>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
     </ul>
-    <h3>Essential Links</h3>
+    <h3 v-colorLi>Essential Links</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li><a v-colorLi href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a v-colorLi href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a v-colorLi href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a v-colorLi href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a v-colorLi href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
     </ul>
-    <h3>Ecosystem</h3>
-    <ul>
+    <h3 v-colorLi>Ecosystem</h3>
+    <ul v-second:column>
       <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
       <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
